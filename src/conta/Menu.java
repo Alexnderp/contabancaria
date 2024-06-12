bc397b9c452b7fd2375063a03690ae357a90eaf5
Alexander Pereira <xandi9731@gmail.com>
2024-06-12 16:00:28 -0300

    Feat: Task - 01

---
+++ src/conta/Menu.java
@@ -0,0 +1,92 @@
+package conta;
+
+import conta.util.Cores;
+
+import java.util.Scanner;
+
+public class Menu {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        int option;
+        while (true){
+            System.out.printf("""
+                    %s %s*********************************************************
+                    
+                                        Generation Bank
+                    
+                    *********************************************************
+                    
+                                   1 - Criar conta
+                                   2 - Listar Todas as Contas
+                                   3 - Buscar Conta Por Número
+                                   4 - Atualizar Dados da Conta
+                                   5 - Apagar Conta
+                                   6 - Sacar
+                                   7 - Depositar
+                                   8 - Transferir valores entre Contas
+                                   9 - Sair
+                    *********************************************************
+                    Entre com a opção desejada:
+                    %s
+                    
+                    """, Cores.TEXT_YELLOW, Cores.ANSI_BLACK_BACKGROUND, Cores.TEXT_RESET);
+
+            option = sc.nextInt();
+
+            if (option == 9) {
+                System.out.println(Cores.TEXT_WHITE_BOLD + "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+                sobre();
+                sc.close();
+                System.exit(0);
+            }
+
+            switch (option) {
+                case 1:
+                    System.out.println(Cores.TEXT_WHITE_BOLD + "Criar Conta\n\n");
+
+                    break;
+                case 2:
+                    System.out.println(Cores.TEXT_WHITE_BOLD + "Listar todas as Contas\n\n");
+
+                    break;
+                case 3:
+                    System.out.println(Cores.TEXT_WHITE_BOLD + "Consultar dados da Conta - por número\n\n");
+
+                    break;
+                case 4:
+                    System.out.println(Cores.TEXT_WHITE_BOLD + "Atualizar dados da Conta\n\n");
+
+                    break;
+                case 5:
+                    System.out.println(Cores.TEXT_WHITE_BOLD + "Apagar a Conta\n\n");
+
+                    break;
+                case 6:
+                    System.out.println(Cores.TEXT_WHITE_BOLD + "Saque\n\n");
+
+                    break;
+                case 7:
+                    System.out.println(Cores.TEXT_WHITE_BOLD + "Depósito\n\n");
+
+                    break;
+                case 8:
+                    System.out.println(Cores.TEXT_WHITE_BOLD + "Transferência entre Contas\n\n");
+
+                    break;
+                default:
+                    System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n");
+                    break;
+            }
+        }
+    }
+
+    public static void sobre() {
+        System.out.println("\n*********************************************************");
+        System.out.println("Projeto Desenvolvido por:");
+        System.out.println("Generation Brasil - generation@generation.org");
+        System.out.println("github.com/conteudoGeneration");
+        System.out.println("*********************************************************");
+    }
+}
