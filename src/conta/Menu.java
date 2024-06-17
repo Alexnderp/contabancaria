213f55bf7a61b17fae3ce112da73a09305b3d92e
Alexander Pereira <xandi9731@gmail.com>
2024-06-17 17:29:59 -0300

    Feat: Task - 04

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,32 +1,26 @@
 package conta;
 
+import conta.controller.ContaController;
 import conta.model.ContaCorrente;
 import conta.model.ContaPoupanca;
 import conta.util.Cores;
+import conta.util.Functions;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
+
 
 public class Menu {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ContaController accounts = new ContaController();
+        int option, number, agency, type, birthdate;
+        String owner;
+        double balance, limit;
 
 
-        ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "José da Silva", 0.0f, 1000.0f);
-        cc1.view();
-        cc1.withdraw(12000.0f);
-        cc1.view();
-        cc1.deposit(5000.0f);
-        cc1.view();
 
-        ContaPoupanca cp1 = new ContaPoupanca(2, 123, 2, "Maria dos Santos", 100000.0f, 15);
-        cp1.view();
-        cp1.withdraw(1000.0f);
-        cp1.view();
-        cp1.deposit(5000.0f);
-        cp1.view();
-
-
-        int option;
         while (true){
             System.out.printf("""
                     %s%s*********************************************************
@@ -49,61 +43,93 @@ public class Menu {
                     %s
                     
                     """, Cores.TEXT_YELLOW, Cores.ANSI_BLACK_BACKGROUND, Cores.TEXT_RESET);
+            try {
+                option = sc.nextInt();
+            }catch (InputMismatchException e){
+                System.out.println("\nDigite valores inteiros!");
+                sc.nextLine();
+                option=0;
+            }
 
-            option = sc.nextInt();
 
             if (option == 9) {
                 System.out.println(Cores.TEXT_WHITE_BOLD + "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
-                sobre();
+                Functions.sobre();
                 sc.close();
                 System.exit(0);
             }
 
             switch (option) {
-                case 1:
+                case 1 ->{
                     System.out.println(Cores.TEXT_WHITE_BOLD + "Criar Conta\n\n");
+                    System.out.println("Insira o numero da agencia: ");
+                    agency = sc.nextInt();
+                    System.out.println("Insira o nome do titular: ");
+                    sc.skip("\\R?");
+                    owner = sc.nextLine();
+                    do {
+                        System.out.println("Informe o tipo da conta: (1 - CC ou 2 - CP) ");
+                        type = sc.nextInt();
+                        if (type < 1 && type > 2) System.out.println("Tipo de conta invalido!");
+                    }while (type < 1 && type > 2);
 
-                    break;
-                case 2:
+                    System.out.println("Informe o saldo da conta: ");
+                    balance = sc.nextDouble();
+
+                    switch (type){
+                        case 1 -> {
+                            System.out.println("Informe o limite de crédito: ");
+                            limit = sc.nextDouble();
+                            accounts.register(new ContaCorrente(Functions.generateNumber(),agency,type,owner,balance,limit));
+                        }
+                        case 2-> {
+                            System.out.println("Informe o aniversário da conta: ");
+                            birthdate = sc.nextInt();
+                            accounts.register(new ContaPoupanca(Functions.generateNumber(),agency,type,owner,balance,birthdate));
+                        }
+
+                    }
+
+                    Functions.keyPress();
+                }
+
+                case 2 ->{
                     System.out.println(Cores.TEXT_WHITE_BOLD + "Listar todas as Contas\n\n");
-
-                    break;
-                case 3:
+                    accounts.listAll();
+                    Functions.keyPress();
+                }
+                case 3 ->{
                     System.out.println(Cores.TEXT_WHITE_BOLD + "Consultar dados da Conta - por número\n\n");
-
-                    break;
-                case 4:
+                    Functions.keyPress();
+                }
+                case 4 ->{
                     System.out.println(Cores.TEXT_WHITE_BOLD + "Atualizar dados da Conta\n\n");
-
-                    break;
-                case 5:
+                    Functions.keyPress();
+                }
+                case 5 -> {
                     System.out.println(Cores.TEXT_WHITE_BOLD + "Apagar a Conta\n\n");
-
-                    break;
-                case 6:
+                    Functions.keyPress();
+                }
+                case 6 -> {
                     System.out.println(Cores.TEXT_WHITE_BOLD + "Saque\n\n");
-
-                    break;
-                case 7:
+                    Functions.keyPress();
+                }
+                case 7 ->{
                     System.out.println(Cores.TEXT_WHITE_BOLD + "Depósito\n\n");
-
-                    break;
-                case 8:
+                    Functions.keyPress();
+                }
+                case 8 ->{
                     System.out.println(Cores.TEXT_WHITE_BOLD + "Transferência entre Contas\n\n");
+                    Functions.keyPress();
 
-                    break;
-                default:
-                    System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n");
-                    break;
+                }
+                default ->{
+                    System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n" + Cores.TEXT_RESET);
+                    Functions.keyPress();
+                }
             }
         }
     }
 
-    public static void sobre() {
-        System.out.println("\n*********************************************************");
-        System.out.println("Projeto Desenvolvido por:");
-        System.out.println("Generation Brasil - generation@generation.org");
-        System.out.println("github.com/conteudoGeneration");
-        System.out.println("*********************************************************");
-    }
+
 }
